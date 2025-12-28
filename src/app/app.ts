846b20aee78357aaceb95bc15f79859c2d75db8b
viklop <viklop7025@gmail.com>
2025-12-28 07:33:08 -0500

    feat: crear footer.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, Footer],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('country-spa');
+    protected readonly title = signal('country-spa');
 }
--- src/app/shared/pages/home-page/home-page.ts
+++ src/app/shared/pages/home-page/home-page.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-home-page',
-  imports: [],
-  templateUrl: './home-page.html',
-  styleUrl: './home-page.css',
+    selector: 'app-home-page',
+    imports: [RouterModule],
+    templateUrl: './home-page.html',
+    styleUrl: './home-page.css',
 })
-export class HomePage {
-
-}
+export class HomePage {}
