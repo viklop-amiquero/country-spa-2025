ff82321762ab4d55ebf033e9376f80bef7b72b72
viklop <viklop7025@gmail.com>
2025-12-28 07:59:13 -0500

    feat: implementar la navegación.

---
+++ src/app/country/components/top-menu/top-menu.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'country-top-menu',
+    imports: [RouterModule],
+    templateUrl: './top-menu.html',
+    styleUrl: './top-menu.css',
+})
+export class TopMenu {}
--- src/app/country/layouts/country-layout/country-layout.ts
+++ src/app/country/layouts/country-layout/country-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TopMenu } from '../../components/top-menu/top-menu';
 
 @Component({
     selector: 'app-country-layout',
-    imports: [RouterModule],
+    imports: [RouterModule, TopMenu],
     templateUrl: './country-layout.html',
     styleUrl: './country-layout.css',
 })
